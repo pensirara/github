6a2a572cb58bfc7c7d66a35f1a3924078e052f26
pensirara <pensirara@naver.com>
2017-07-25 09:50:11 +0900

    Revert "ra"
    This reverts commit 7ffa40afe24b96b0e8224127fd5da68c346a5d03.

--- webapp/hello_flask.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app=Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "dsfsdfHello RasberryPi with Flask!!"
-if __name__=="__main__":
-    app.run(host='0.0.0.0',port=1234,debug=True)
-    
